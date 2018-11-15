d73ce71f5c49e1c95a6a55698515c5eee09b6848
raj <raj@cura.tech>
2018-11-15 06:38:49 +0530

    Updates

---
+++ precache-manifest.6fcda99b2646209e690a33beb6fd9218.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "74556ee42cfbdc90fe1550cdc2f0c9a0",
+    "url": "/wakecap/static/media/wakecap.74556ee4.png"
+  },
+  {
+    "revision": "f5090afbd4cabc8761bd1e2806c9d048",
+    "url": "/wakecap/static/media/identification.f5090afb.png"
+  },
+  {
+    "revision": "13c79c7cdbf251a024776c102f119ae6",
+    "url": "/wakecap/static/media/helmet.13c79c7c.png"
+  },
+  {
+    "revision": "ee7188e5a811ee9de967bc03d353d4f1",
+    "url": "/wakecap/static/media/hamburger-icon.ee7188e5.png"
+  },
+  {
+    "revision": "ac24e064ad04837f8c07",
+    "url": "/wakecap/static/js/runtime~main.ac24e064.js"
+  },
+  {
+    "revision": "4a398cca6fbd76b9b328",
+    "url": "/wakecap/static/js/main.4a398cca.chunk.js"
+  },
+  {
+    "revision": "f5b2398f2ea5b609a409",
+    "url": "/wakecap/static/js/1.f5b2398f.chunk.js"
+  },
+  {
+    "revision": "4a398cca6fbd76b9b328",
+    "url": "/wakecap/static/css/main.b36566b6.chunk.css"
+  },
+  {
+    "revision": "5c76210eb940033c5dfe6361c9b91987",
+    "url": "/wakecap/index.html"
+  }
+];
